16c01a22a25d8d3bd4223461a57dd67364132526
Can Toraman <cantoraman@gmail.com>
2018-05-29 18:51:27 +0100

    bear speaks and can tell if it's hungry or not

--- bear.rb
+++ bear.rb
@@ -7,4 +7,20 @@ class Bear
     @stomach = []
   end
 
+
+
+
+def catch_fish(river, amount)
+    caught_fish = river.give_fish(amount)
+    @stomach.concat(caught_fish)
+end
+
+def roar
+  puts "ROAAAR!!!"
+end
+
+def food_count
+  return @stomach.length
+end
+
 end
